20b6e37ba3cc76e42885bdd54ac97962f947f6aa
soufiane ounida <soufianeounidainfo@gmail.com>
2020-09-25 01:25:20 +0100

    project structure + installation of chartjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartsComponent } from './components/charts/charts.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { GanttComponent } from './components/gantt/gantt.component';
+import { SchedulerComponent } from './components/scheduler/scheduler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartsComponent,
+    DashboardComponent,
+    GanttComponent,
+    SchedulerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/gantt/gantt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gantt',
+  templateUrl: './gantt.component.html',
+  styleUrls: ['./gantt.component.css']
+})
+export class GanttComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
